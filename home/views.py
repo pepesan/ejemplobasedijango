3f9c9f689b1a71b9489d27199442291f8e52d16f
David Vaquero <pepesan@gmail.com>
2018-05-18 11:41:06 +0200

    directorio unificado de rutas a plantillas por modulo

--- home/views.py
+++ home/views.py
@@ -1,5 +1,13 @@
 from django.shortcuts import render
+from django.template import loader
 from django.http import HttpResponse
 
+"""
 def index(request):
     return HttpResponse("Estás en la página principal. </br><a href='/polls'>Enlace a pools</a>")
+"""
+def index(request):
+    titulo="Mi título molón"
+    template = loader.get_template('home/index.html')
+    context = {'titulo': titulo}
+    return HttpResponse(template.render(context, request))
