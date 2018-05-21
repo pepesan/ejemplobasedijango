f13e99ebeb24d64ba28da3d2191c688b5a2d5c6f
David Vaquero <pepesan@gmail.com>
2018-05-21 17:23:42 +0200

    mejora del uso de ficheros estáticos en módulos

--- ejemplobasedijango/urls.py
+++ ejemplobasedijango/urls.py
@@ -21,3 +21,9 @@ urlpatterns = [
     path('polls/', include('polls.urls')),
     path('admin/', admin.site.urls),
 ]
+
+from django.conf import settings
+from django.conf.urls.static import static
+
+urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+
